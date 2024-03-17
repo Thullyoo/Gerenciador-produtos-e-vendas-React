2c20a65f6d807d6df5bbaa9c0776514c9b26129d
Thullyoo <123775158+Thullyoo@users.noreply.github.com>
2024-03-17 12:15:52 -0300

    feat: algumas criações pendentes

---
+++ src/pages/Vendas.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/Header";
+
+
+const Vendas = () => {
+    return(
+        <>
+        <header><Header></Header></header>
+        </>
+    )
+}
+
+export default Vendas;
